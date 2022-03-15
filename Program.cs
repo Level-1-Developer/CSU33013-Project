33bab21b9e6042df46fe531f58c66926087bd4d6
Luke Secky <77547327+Level-1-Developer@users.noreply.github.com>
2022-03-15 17:33:41 +0000

    Add files via upload
    Base code to connect to database

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Npgsql;
+
+namespace PGDemo // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var connString = "Host=localhost;Username=postgres;Password=Helloworld;Database=postgres";
+
+            await using var conn = new NpgsqlConnection(connString);
+            await conn.OpenAsync();
+
+        }
+    }
+
+}
+    
