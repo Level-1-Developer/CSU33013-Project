8b4af980e5e7e2e9c548e42ff4033b5bedfda81e
Luke Secky <77547327+Level-1-Developer@users.noreply.github.com>
2022-03-15 18:24:03 +0000

    Update Program.cs
    Added some functionality.

--- Program.cs
+++ Program.cs
@@ -11,6 +11,24 @@ namespace PGDemo // Note: actual namespace depends on the project name.
 
             await using var conn = new NpgsqlConnection(connString);
             await conn.OpenAsync();
+            
+            //below code needs an existing table/relation called data (can be changed) to run
+            
+            /* Insert some data
+            await using (var cmd = new NpgsqlCommand("INSERT INTO data (some_field) VALUES (@p)", conn))
+            {
+                cmd.Parameters.AddWithValue("p", "Hello world");
+                await cmd.ExecuteNonQueryAsync();
+            }
+
+             Retrieve all rows
+            await using (var cmd = new NpgsqlCommand("SELECT some_field FROM data", conn))
+            await using (var reader = await cmd.ExecuteReaderAsync())
+            {
+                while (await reader.ReadAsync())
+                    Console.WriteLine(reader.GetString(0));
+            }
+            */
 
         }
     }
