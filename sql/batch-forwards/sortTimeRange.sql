d27887337733f6263cc406cd76b5cd557fbff95c
Varjak Wolfe <78870995+varjakw@users.noreply.github.com>
2022-03-25 15:28:37 +0000

    Update sortTimeRange.sql

--- sql/batch-forwards/sortTimeRange.sql
+++ sql/batch-forwards/sortTimeRange.sql
@@ -1,3 +1,4 @@
 //needs to be tested
+//suppsoed to show batch-forwards from the past hour. should do for 6 hours, 12 hours and 24 hours also
 SELECT * FROM batch-forwards
 WHERE DATEDIFF(HOUR, completed_at , GETDATE()) <= 1;
