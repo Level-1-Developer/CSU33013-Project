fa19a790dc2ace519e3ca6f308ef4790d87f532e
Luke Secky <77547327+Level-1-Developer@users.noreply.github.com>
2022-03-31 10:43:39 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,35 +1,53 @@
 ﻿using System;
+using System.Collections;
 using Npgsql;
 
-namespace PGDemo // Note: actual namespace depends on the project name.
+namespace ReturnDataAsArray // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
         static async Task Main(string[] args)
         {
             var connString = "Host=localhost;Username=postgres;Password=Helloworld;Database=postgres";
+
             await using var conn = new NpgsqlConnection(connString);
             await conn.OpenAsync();
 
             //below code needs an existing table/relation called data (can be changed) to run
-            /* Insert some data
-            await using (var cmd = new NpgsqlCommand("INSERT INTO data (some_field) VALUES (@p)", conn))
-            {
-                cmd.Parameters.AddWithValue("p", "Hello world");
-                await cmd.ExecuteNonQueryAsync();
-            }
 
-             Retrieve all rows
-            await using (var cmd = new NpgsqlCommand("SELECT some_field FROM data", conn))
+
+            // Retrieve all rows
+            int numberOfEntries = 6;
+            int numberOfColumns = 3;
+            int n = numberOfColumns;
+            String[] results = new string[numberOfEntries]; //also need to figure out proper lenghth
+
+            
+            await using (var cmd = new NpgsqlCommand("SELECT * FROM actionitems", conn))
             await using (var reader = await cmd.ExecuteReaderAsync())
             {
+                Console.WriteLine("Displaying results");
+
+                List<string> ID = new List<string>();
+                List<string> email = new List<string>();
                 while (await reader.ReadAsync())
-                    Console.WriteLine(reader.GetString(0));
+                {
+
+                    Console.WriteLine(reader[0]);
+                    ID.Add(reader[0].ToString());
+                    email.Add(reader[1].ToString());
+                    
+
+                    //Check the data schema and proceed 
+                  
+                }
+                Console.WriteLine(ID[0] + " has the email of " + email[0]);
+               
             }
-            */
+
 
         }
     }
 
 }
-    
+
