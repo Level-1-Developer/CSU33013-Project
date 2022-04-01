5e7751b2408ba6640dbaca0cb4ca6ecb99a67694
Tom Moroney <moroneyt@tcd.ie>
2022-04-01 21:39:24 +0100

    Added Non-Functional Search Bar

---
+++ web-app/wwwroot/scripts/app.ts
@@ -0,0 +1,7 @@
+function sayHello() {
+    const compiler = (document.getElementById("compiler") as HTMLInputElement)
+        .value;
+    const framework = (document.getElementById("framework") as HTMLInputElement)
+        .value;
+    return `Hello from ${compiler} and ${framework}!`;
+}
