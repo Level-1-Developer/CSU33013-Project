c2c44f83ec7e6e05489a04f6e5fd63559e453502
Tom Moroney <moroneyt@tcd.ie>
2022-04-03 16:44:28 +0100

    Added Batch Files Page and Fixed Styling Issues

---
+++ web-app/Pages/BatchFiles.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace web_app.Pages
+{
+    public class BatchFilesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
